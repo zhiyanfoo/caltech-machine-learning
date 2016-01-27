5c91196c2cb837c2435ce512f6feb4fd6cbffb93
Zhiyan Foo <zhiyanfoo@gmail.com>
2016-01-27 21:59:56 +0800

    using numpy to answer ass2, ass1 answered with np.

--- ass2/np_percepton.py
+++ ass2/np_percepton.py
@@ -4,6 +4,16 @@ from itertools import chain
 np.random.seed(0)
 
 def n_random_datapoint(n):
+    '''
+    [[ 1.          0.09762701  0.43037873]
+     [ 1.          0.20552675  0.08976637]
+     [ 1.         -0.1526904   0.29178823]
+     [ 1.         -0.12482558  0.783546  ]
+     [ 1.          0.92732552 -0.23311696]
+     [ 1.          0.58345008  0.05778984]
+     [ 1.          0.13608912  0.85119328]
+     [ 1.         -0.85792788 -0.8257414 ]]
+    '''
     return np.insert(np.random.uniform(-1,1, size=(n,2)), 0, 1, axis=1)
 
 def rand_line():
@@ -17,32 +27,41 @@ def create_lin_target_function(m,c):
         return x*m + c
     return lin_target_function
 
+
+def classify_data(raw_data, target_function):
+    classified = np.array([ classify_point(data_point, target_function) for data_point in raw_data ])
+    return {'raw' : raw_data, 'classified' : classified}
+
+def classify_data_linear_binary_random(raw_data):
+    m, c = rand_line()
+    target_function = create_lin_target_function(m, c)
+    data = classify_data(raw_data, target_function)
+    return data, target_function
+
 def classify_point(data_point, target_function):
     if data_point[2] - target_function(data_point[1]) < 0:
         return -1
     else:
         return 1
 
-def classify_data(raw_data, target_function):
-    return np.array([ [data_point, classify_point(data_point, target_function)] for data_point in raw_data ])
-
 def binary_percepton(data):
-    weight_vectors = np.zeros(len(data[0,0]))
+    weight_vectors = np.zeros(len(data['raw'][0]))
     weight, iterations = update(weight_vectors, data, 0)
     return weight, iterations
 
 def update(weight, data, iterations):
-    mis_point = a_misclassified_point(data, weight)
-    if mis_point is None:
+    mis_index = a_misclassified_point(data, weight)
+    if mis_index is None:
         return weight, iterations
-    new_weight = weight + mis_point[1] * mis_point[0]
+    new_weight = weight + data['raw'][mis_index] * data['classified'][mis_index]
+    # print(new_weight)
     return update(new_weight, data, iterations + 1)
 
 def a_misclassified_point(data, weight):
-    start = np.random.randint(0, len(data) - 1)
-    for i in chain(range(0, start), range(start,len(data))):
-        if sign(data[i,0], weight) != data[i,1]:
-            return data[i]
+    start = np.random.randint(0, len(data['raw']) - 1)
+    for i in chain(range(0, start), range(start, len(data['raw']))):
+        if sign(data['raw'][i], weight) != data['classified'][i]:
+            return i
     return None
 
 def sign(x,w):
@@ -55,25 +74,44 @@ def sign(x,w):
         return 0
 
 def linear_percepton(data):
-    x = data[:,0]
+    x = data['raw']
+    y = data['classified']
     print("x")
     print(x)
-    y = np.matrix(data[:,1])
-    # print("y")
-    # print(y)
-    xt_x = x.transpose() * x
+    print("y")
+    print(y)
+    xt_x = x.transpose().dot(x)
+    print('xt_x')
     print(xt_x)
-    # xt_y = x.transpose() * y
-    # print(xt_y)
-    # inv_xt_x = np.linalg.inv(xt_x)
-    # return np.dot(np.linalg.inv(xt_x), np.dot(x.transpose(), y))
+    xt_y = x.transpose().dot(y)
+    print('xt_y')
+    print(xt_y)
+    inv_xt_x = np.linalg.inv(xt_x)
+    print('inv_xt_x')
+    print(inv_xt_x)
+    return inv_xt_x.dot(xt_y)
 
-def check_classification(data, lin_target_function):
+def check_classification(data, lin_target_function, weight=None):
     import matplotlib.pyplot as plt
+    
+    if weight != None:
+        def color(i):
+            return sign(data['raw'][i], weight)
+    else:
+        def color(i):
+            return data['classified'][i]
+
+
     plt.plot([-1, 1], [lin_target_function(-1), lin_target_function(1)], '-')
     plt.plot([-1,1],[lin_target_function(-1),lin_target_function(1)], '-')
-    xy_plus = [ [data_point[0][1], data_point[0][2]] for data_point in data if data_point[1] == 1]
-    xy_minus = [ [data_point[0][1], data_point[0][2]] for data_point in data if data_point[1] == -1]
+    # xy_plus = [ [data['raw'][i,1], data['raw'][i,2]] for i in range(len(data['raw'])) if data['classified'][i] == 1]
+    xy_plus = [ [data['raw'][i,1], data['raw'][i,2]] for i in range(len(data['raw'])) if color(i) == 1]
+    print('xy_plus')
+    print(xy_plus)
+    # xy_minus = [ [data['raw'][i,1], data['raw'][i,2]] for i in range(len(data['raw'])) if data['classified'][i]  == -1]
+    xy_minus = [ [data['raw'][i,1], data['raw'][i,2]] for i in range(len(data['raw'])) if color(i)  == -1]
+    print('xy_minus')
+    print(xy_minus)
     p1 = zip(*xy_plus)
     p2 = zip(*xy_minus)
 
@@ -85,17 +123,46 @@ def check_classification(data, lin_target_function):
     plt.axis([-1, 1, -1, 1])
     plt.show()
 
+def trial(in_sample, out_sample):
+    raw_data = n_random_datapoint(out_sample)
+    training_indices = np.random.choice(out_sample, size=in_sample, replace=False)
+    print('training_indices')
+    print(training_indices)
+    training_data = raw_data[training_indices,:]
+    print('training_data')
+    print(training_data)
+    data, target_function = classify_data_linear_binary_random(raw_data)
+    linear_weight = linear_percepton(data)
+    return check_error(training_data), return check_error(raw_data)
+
+
+
+def check_error(data, linear_weight):
+    linear_classification = [ sign(x, linear_weight) for x in data['raw'] ]
+    n_misclassified_points = len(data['classified']) - sum(linear_classification == data['classified'])
+    check_classification(data, target_function, linear_weight)
+    print(n_misclassified_points)
+    return n_misclassified_points / len(data['raw'])
+
+
 def main():
-    raw_data = n_random_datapoint(8)
+    # raw_data = n_random_datapoint(140)
+    # print('raw_data')
     # print(raw_data)
-    m, c = rand_line()
+    # m, c = rand_line()
     # print(m ,c)
-    target_function = create_lin_target_function(m, c)
-    data = classify_data(raw_data, target_function)
+    # target_function = create_lin_target_function(m, c)
+    # data = classify_data(raw_data, target_function)
+    # print('data')
     # print(data)
+    # print('binary_percepton')
     # print(binary_percepton(data))
-    print(linear_percepton(data))
-    # check_classification(data, target_function)
+    # print('linear_percepton')
+    # lin_per = linear_percepton(data)
+    # print(lin_per)
+    # check_classification(data, target_function, lin_per)
+    trial(100, 1000)
+
 
 if __name__ == "__main__":
     main()
