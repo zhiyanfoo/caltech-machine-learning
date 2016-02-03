9c6efc0c1d7b68af5b23a02b104300fb91c73665
Zhiyan Foo <zhiyanfoo@gmail.com>
2016-02-03 19:28:25 +0800

    Finish question 7

--- ass3/hw3.py
+++ ass3/hw3.py
@@ -15,7 +15,7 @@ ans5 = 'c'
 
 # find the growth function of the two interval hypothesis set
 def growth_function_two_interval(n):
-    return first_black(n)
+    return 2 * (first_black(n) + comb(n-1,2) + n)
 
 def first_black(n):
     total = 0
@@ -36,10 +36,28 @@ def second_black(n):
     return n - 1
 
 def growth_function_two_interval_analytical(n):
-    return sum([ comb(n-i-1, 2) for i in range(1, n-3+1) ])
+    return 2 * (sum([ comb(n-i-1, 2) for i in range(1, n-3+1) ]) + comb(n-1,2) + n)
+
+def growth_function_option_a(n):
+    return comb(n+1, 4)
+
+def growth_function_option_b(n):
+    return comb(n+1, 2) + 1
+
+def growth_function_option_c(n):
+    return comb(n+1, 4) + comb(n+1, 2) + 1
+
+def growth_function_option_d(n):
+    return comb(n+1, 4) + comb(n+1, 3) + comb(n+1, 2) + comb(n+1, 1) + 1
 
 def question_seven():
-    return [ (growth_function_two_interval(n), growth_function_two_interval_analytical(n)) for n in range(4, 14) ] 
+    return [ (growth_function_two_interval(n),
+        growth_function_two_interval_analytical(n),
+        growth_function_option_a(n),
+        growth_function_option_b(n),
+        growth_function_option_c(n),
+        growth_function_option_d(n),
+        ) for n in range(4, 14) ] 
 
 
 def main():
